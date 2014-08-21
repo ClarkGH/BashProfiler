eb3e82949cbb3ee4f235747954966a927a6592cc
Patrick Vilhena <plvilhena@gmail.com>
2014-08-21 13:38:09 -0700

    updates specs

--- spec/controllers/command_controller_spec.js
+++ spec/controllers/command_controller_spec.js
@@ -4,23 +4,25 @@ describe("Command Controller", function(){
 
     var ctrl, scope;
 
-    var LogMock = {
+    beforeEach(function() {
+        LogMock = {
 
-        terminalHistory: [],
+            terminalHistory: [],
 
-        entries: function(book) {
-            return this.terminalHistory
-        },
+            entries: function(book) {
+                return this.terminalHistory
+            },
 
-        clear: function() {
-            this.terminalHistory = []
-        },
+            clear: function() {
+                this.terminalHistory = []
+            },
 
-        addEntry: function( command, returnValue, currentDir ) {
-            this.terminalHistory.push( {command: command, returnValue: returnValue, currentDir: currentDir} );
-        }
+            addEntry: function( command, returnValue, currentDir ) {
+                this.terminalHistory.push( {command: command, returnValue: returnValue, currentDir: currentDir} );
+            }
 
-   };
+       };
+   });
 
     beforeEach( inject( function ( $rootScope, $controller ) {
 
@@ -35,35 +37,45 @@ describe("Command Controller", function(){
 
     it(  "should identify whether a command is invalid", function(){
         scope.textInputField = "Foo";
-        expect( scope.parseInput() ).toBe( "Foo: command not found" );
+        expect( scope.parseInput() ).toEqual( "Foo: command not found" );
     });
 
     it(  "should be able to create a directory", function(){
         scope.textInputField = "mkdir Foo";
         scope.parseInput();
-        expect( scope.ls().split(" ").last() ).toBe( "Foo" );
+        expect( scope.ls().split(" ").last() ).toEqual( "Foo" );
     });
 
     it( "should add each command to the history", function(){
-        expect( LogMock.entries().length ).toBe( 2 )
+        scope.textInputField = "mkdir Foo";
+        scope.parseInput();
+        scope.textInputField = "foo";
+        scope.parseInput();
+
+        expect( LogMock.entries().length ).toEqual( 2 )
     });
 
     it( "should clear the history", function(){
+        scope.textInputField = "mkdir Foo";
+        scope.parseInput();
+        scope.textInputField = "foo";
+        scope.parseInput();
         scope.clear();
-        expect( LogMock.entries().length ).toBe( 0 );
+
+        expect( LogMock.entries().length ).toEqual( 0 );
     });
 
     it(  "should create a directory in another directory without changing the current path", function(){
         scope.textInputField = "mkdir ../../Foo";
         scope.parseInput();
-        expect( scope.pwd() ).toBe("/root/Users/Connor");
+        expect( scope.pwd() ).toEqual("/root/Users/Connor");
 
         scope.cd('../..');
-        expect( scope.ls().split(" ").last() ).toBe( "Foo" );
+        expect( scope.ls().split(" ").last() ).toEqual( "Foo" );
     });
 
     it(  "should return the current directory", function(){
-        expect( scope.pwd() ).toBe( '/root/Users/Connor' );
+        expect( scope.pwd() ).toEqual( '/root/Users/Connor' );
     });
 
     it(  "should be able to change directories", function(){
@@ -71,11 +83,11 @@ describe("Command Controller", function(){
         scope.cd("../Clark/Applications");
         scope.$apply();
 
-        expect( scope.currentPath ).toBe( '/root/Users/Clark/Applications' );
+        expect( scope.currentPath ).toEqual( '/root/Users/Clark/Applications' );
     });
 
     it( "should not change to a directory that doesn't exist", function(){
-        expect( scope.cd("../Patrick/Foo") ).toBe( "No such file or directory: ../Patrick/Foo" );
+        expect( scope.cd("../Patrick/Foo") ).toEqual( "No such file or directory: ../Patrick/Foo" );
     })
 
 })
